dd28ae5f994fa27d6f2f35531afa7c6a4f539f1b
SuverBoi <dmitro.kovalik12345@gmail.com>
2023-10-21 18:08:25 +0300

    g

---
+++ 20.10.2023/20.10.2023.cpp
@@ -0,0 +1,27 @@
+// 20.10.2023.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+
+
+    //час в секундах задача 1
+
+    //int time_seconds;
+    //cout << "Say time in seconds to get TIME:" << endl;
+    //cin >> time_seconds;
+    //cout << time_seconds / 3600 << ":" << time_seconds % 3600 / 60 << ":" << time_seconds % 3600 % 60 << endl;
+ 
+    //чотиризначне число задача 2
+
+    //int number = 4567; any number can be here
+    //int firstDigit = number % 10;
+    //number /= 10;
+    //int secondDigit = number % 10;
+    //number /= 10;
+    //int thirdDigit = number % 10;
+    //number /= 10;
+    //int fourthDigit = number % 10;
+    //cout << firstDigit << secondDigit << thirdDigit << fourthDigit << endl;
+}
